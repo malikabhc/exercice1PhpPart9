585ce6b105fa4ee26699477bca9d53b026fcbf46
Malikabhc <malika.bouhcine@gmail.com>
2018-08-27 13:41:38 +0200

    exercice1-9

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="style.css" />
+        <title>Exercice1/9</title>
+    </head>
+    <body>
+        <p><?= //Affichage de la date du jour. Le Y est en majuscule pour afficher 2018 s'il est en minuscule il affiche seulement 18.
+        date('d/m/Y'); ?></p>
+    </body>
+</html>
